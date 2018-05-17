9a302c1f03305fa504890964779c39b87bfcbeac
qingmingzi <qingmingzi@hotmail.com>
2018-05-17 09:45:02 +0800

    改造示例为RESTful API

---
+++ README.md
@@ -0,0 +1,25 @@
+# neothinsdk_nodejs_test
+
+## init
+
+```
+npm install
+```
+
+## run
+
+```
+node app.js
+```
+
+## Visit NEO thinSDK RESTful API
+
+### GetPublicKeyScriptHash_FromAddress
+```
+http://127.0.0.1:3000/addr2pksh/ALjSnMZidJqd18iQaoCgFun6iqWRm2cVtj
+```
+
+### GetAddressFromScriptHash
+```
+http://127.0.0.1:3000/pksh2addr/ff32bee28f5cfaad170050880f00e559870a6336
+```
--- app.js
+++ app.js
@@ -1,46 +1,73 @@
 var thinNeo = require("nel-neo-thinsdk");
 
-test_1();
-console.log("");
-test_2();
-console.log("");
-test_3();
-console.log("");
-test_4();
-console.log("");
-test_5();
-console.log("");
-test_6();
-console.log("");
-test_7();
+var express = require('express');
+var app = express();
 
-function test_1() {
-    console.log("Cryptography");
-    var addr = "ALjSnMZidJqd18iQaoCgFun6iqWRm2cVtj";
+//NEO thinSDK api
+app.get('/addr2pksh/:addr', function (req, res) {
+    var input = req.params.addr;
+    var output = {
+        addr:input,
+        pksh:addr2pksh(input)
+    }
+    res.end(JSON.stringify(output))
+  });
+app.get('/pksh2addr/:pksh', function (req, res) {
+    var input = req.params.pksh;
+    var output = {
+        pksh:input,
+        addr:pksh2addr(input)
+    }
+    res.end(JSON.stringify(output))
+});
+  
+app.listen(3000, function () {
+    console.log('NEO thinSDK Test listening on port 3000!');
+});
+
+// addr2pksh('ALjSnMZidJqd18iQaoCgFun6iqWRm2cVtj');
+// console.log("");
+// pksh2addr("0x0b193415c6f098b02e81a3b14d0e3b08e9c3f79a");
+// console.log("");
+// pk2addr("02bf055764de0320c8221920d856d3d9b93dfc1dcbc759a560fd42553aa025ba5c");
+// console.log("");
+// test_4();
+// console.log("");
+// test_5();
+// console.log("");
+// test_6();
+// console.log("");
+// test_7();
+
+function addr2pksh(addr) {
+    // console.log("Cryptography");
+    // var addr = "ALjSnMZidJqd18iQaoCgFun6iqWRm2cVtj";
     var uint8 = ThinNeo.Helper.GetPublicKeyScriptHash_FromAddress(addr);
     var hexstr = uint8.reverse().toHexString();
     console.log("addr=" + addr);
     console.log("hex=" + hexstr);
+    return '0x' + hexstr;
 
 }
-function test_2() {
+function pksh2addr(pksh) {
     console.log("Hash2Address");
-    var hexstr = "0x0b193415c6f098b02e81a3b14d0e3b08e9c3f79a";
-    var hashrev = hexstr.hexToBytes();
+    // var hexstr = "0x0b193415c6f098b02e81a3b14d0e3b08e9c3f79a";
+    var hashrev = pksh.hexToBytes();
     var hash = hashrev.reverse();
     var addr = ThinNeo.Helper.GetAddressFromScriptHash(hash);
-    console.log("hex=" + hexstr);
+    console.log("hex=" + pksh);
     console.log("addr=" + addr);
-
+    return addr;
 }
-function test_3() {
+function pk2addr(pk) {
     console.log("Test_Pubkey2Address");
 
-    var pubkey = "02bf055764de0320c8221920d856d3d9b93dfc1dcbc759a560fd42553aa025ba5c";
-    var bytes = pubkey.hexToBytes();
+    //var pubkey = "02bf055764de0320c8221920d856d3d9b93dfc1dcbc759a560fd42553aa025ba5c";
+    var bytes = pk.hexToBytes();
     var addr = ThinNeo.Helper.GetAddressFromPublicKey(bytes);
-    console.log("pubkey=" + pubkey);
+    console.log("pubkey=" + pk);
     console.log("addr=" + addr);
+    return addr;
 }
 function test_4() {
     console.log("WifDecode");
